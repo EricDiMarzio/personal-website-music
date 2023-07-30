ed7673cb1a07bb963bf268ea2f0ffe786999a574
Eric DiMarzio <127116933+EricDiMarzio@users.noreply.github.com>
2023-07-30 12:56:30 -0500

    Reveal section update

--- script.js
+++ script.js
@@ -1,55 +1,62 @@
 'use strict';
 
+const nav = document.querySelector('.navbar');
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 const navLogo = document.getElementById('nav-logo');
 const learnMore = document.getElementById('learn-more');
-const section1Link = document.getElementById('section--1--link');
-const section2Link = document.getElementById('section--2--link');
-const section3Link = document.getElementById('section--3--link');
-const section4Link = document.getElementById('section--4--link');
-const section5Link = document.getElementById('section--5--link');
 
 const header = document.querySelector('.header');
 const section1 = document.getElementById('section--1');
-const section2 = document.getElementById('section--2');
-const section3 = document.getElementById('section--3');
-const section4 = document.getElementById('section--4');
-const section5 = document.getElementById('section--5');
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
   console.log('clicked');
 });
+//Navbar
 
-//Scroll links//
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav-link')) {
+    const link = e.target;
+    const siblings = link.closest('.navbar').querySelectorAll('.nav-link');
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// //Scroll links//
 navLogo.addEventListener('click', function (e) {
   header.scrollIntoView({ behavior: 'smooth' });
 });
 learnMore.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
-section1Link.addEventListener('click', function (e) {
-  section1.scrollIntoView({ behavior: 'smooth' });
+
+document.querySelector('.navbar-links').addEventListener('click', function (e) {
+  if (e.target.classList.contains('nav-link')) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
-section2Link.addEventListener('click', function (e) {
-  section2.scrollIntoView({ behavior: 'smooth' });
-});
-section3Link.addEventListener('click', function (e) {
-  section3.scrollIntoView({ behavior: 'smooth' });
-});
-section4Link.addEventListener('click', function (e) {
-  section4.scrollIntoView({ behavior: 'smooth' });
-});
-section5Link.addEventListener('click', function (e) {
-  section5.scrollIntoView({ behavior: 'smooth' });
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  entry.target.classList.remove('section--hidden');
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.25,
 });
 
-//Product Accordion //
-const accordion = document.getElementsByClassName('product');
-for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-    console.log('working');
-  });
-}
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
